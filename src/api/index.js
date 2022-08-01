07a5459684259aef1ee89536ee10234497e76e31
yeisson giraldo <yeisonnn@gmail.com>
2022-08-01 09:50:31 -0500

    testing working

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
-const BASE = "https://guarded-stream-12358.herokuapp.com/";
+const BASE = 'https://guarded-stream-12358.herokuapp.com/';
 
-export async function testing(){
-    try {
-    const response = await fetch(`${BASE}api/health`)
-    const data = response.json()
-    console.log(data)
-    } catch(error) {
-    throw error
-    }
+export async function testing() {
+  try {
+    const response = await fetch(`${BASE}api/health`);
+    const data = response.json();
+    console.log(data);
+  } catch (error) {
+    throw error;
+  }
 }
-testing()
+testing();
--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      
     </div>
   );
 }
