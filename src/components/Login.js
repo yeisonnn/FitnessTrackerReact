40330756fe7b5c6527c8c1299a3b1b7bccc59602
yeisson giraldo <yeisonnn@gmail.com>
2022-08-01 19:41:33 -0500

    start working on login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import classes from './Login.module.css';
 
 const Login = () => {
   return (
     <>
-      <h1>This Login</h1>
-      <label>Enter a username</label>
-      <input type="text" />
-      <label>Enter password</label>
-      <input type="text" />
-      <button>Login</button>
+      <div className={classes['login-body']}>
+        <div className={classes.box}>
+          <form>
+            <span className={classes['text-center']}>login</span>
+            <div className={classes['input-container']}>
+              <input type="text" />
+              <label>Username</label>
+            </div>
+            <div className={classes['input-container']}>
+              <input type="text" />
+              <label>Password</label>
+            </div>
+            <button type="button" className={classes.btn}>
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
