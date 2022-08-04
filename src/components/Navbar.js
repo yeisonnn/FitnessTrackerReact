6261870303dcf0ed4e5867344bb5029a4689da8e
yeisson giraldo <yeisonnn@gmail.com>
2022-08-04 11:28:56 -0500

    navbar signout working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,75 @@
 import classes from './Navbar.module.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { IoHomeSharp } from 'react-icons/io5';
 import { CgGym } from 'react-icons/cg';
 import { FaUserCheck } from 'react-icons/fa';
+import { GiRunningNinja } from 'react-icons/gi';
+import { BsClockHistory } from 'react-icons/bs';
+import { getCurrentData, clearCurrentData } from '../utils/auth';
 
 const Navbar = () => {
+  const user = getCurrentData('username');
+  const navigate = useNavigate();
+
+  const signupHandler = () => {
+    clearCurrentData();
+    navigate('/');
+  };
+
   return (
     <nav className={classes.navbar}>
-      <ul className={classes.list}>
-        <li>
-          <Link to="/">
-            <IoHomeSharp />
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/activities">
-            <CgGym />
-            Activities
-          </Link>
-        </li>
-        <li>
-          <Link to="/Login">
-            <FaUserCheck />
-            Login
-          </Link>
-          <Link to="/myRoutines">
-            <FaUserCheck />
-            My Routines
-          </Link>
-        </li>
-      </ul>
+      {user ? (
+        <ul className={classes.list}>
+          <li>
+            <Link to="/">
+              <IoHomeSharp />
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/activities">
+              <CgGym />
+              Activities
+            </Link>
+          </li>
+          <li>
+            <Link to="/routines">
+              <GiRunningNinja />
+              Routines
+            </Link>
+          </li>
+          <li>
+            <Link to="/myRoutines">
+              <BsClockHistory />
+              My Routines
+            </Link>
+          </li>
+          <button className={classes.signup} onClick={signupHandler}>
+            Sign out
+          </button>
+        </ul>
+      ) : (
+        <ul className={classes.list}>
+          <li>
+            <Link to="/">
+              <IoHomeSharp />
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/activities">
+              <CgGym />
+              Activities
+            </Link>
+          </li>
+          <li>
+            <Link to="/routines">
+              <GiRunningNinja />
+              Routines
+            </Link>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 };
